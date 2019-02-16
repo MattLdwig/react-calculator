435e3a69c7510329d767c96de32f60eb6a1c07ce
MattLdwig <mattldwig@gmail.com>
2019-02-16 10:02:53 +0100

    feat^($core): Setup UI: - Create Calculator component - Setup buttons - Add basic styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Calculator from './Components/Calculator'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-
-        </header>
+          <Calculator />
       </div>
     );
   }
