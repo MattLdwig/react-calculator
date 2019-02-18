c6438b27753b7dd0eccceabe75ed72054d7eabe9
MattLdwig <mattldwig@gmail.com>
2019-02-18 13:48:34 +0100

    Updates

---
+++ precache-manifest.1bfe6a61debc7e2bbb6f1fd4656593a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6473c9243ea43aad7509",
+    "url": "/react-calculator/static/js/runtime~main.6473c924.js"
+  },
+  {
+    "revision": "3b1a6164d43916365caf",
+    "url": "/react-calculator/static/js/main.3b1a6164.chunk.js"
+  },
+  {
+    "revision": "7834f93e37dad736450a",
+    "url": "/react-calculator/static/js/2.7834f93e.chunk.js"
+  },
+  {
+    "revision": "3b1a6164d43916365caf",
+    "url": "/react-calculator/static/css/main.ff052f74.chunk.css"
+  },
+  {
+    "revision": "20c59d3132eeda8afe39aba72b9dd192",
+    "url": "/react-calculator/index.html"
+  }
+];
